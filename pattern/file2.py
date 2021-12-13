0055fecc28788d252821799152aa14b9724066e0
bhuvanaeswari <bhu_ess@yahoo.co.in>
2021-12-14 00:57:40 +0530

    pattern

---
+++ pattern/file2.py
@@ -0,0 +1,17 @@
+n =int(input("enter the no of rows: "))
+limit=2*n-1
+for row in range(n,0,-1):
+    for space in range(n-1,row-1,-1):
+        print(" ",end=" ")
+    for col in range(1,limit+1):
+        print("*",end= " ")
+    print()
+    limit-=2
+limit=1
+for row in range(1,n+1):
+    for space in range(n-1,row-1,-1):
+        print(" ",end=" ")    
+    for col in range(1,limit+1):
+        print("*",end = " " )
+    print()
+    limit+=2   
